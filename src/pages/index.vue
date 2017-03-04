a0ce8a68f1284c602e9d293d6be63096ed60f34c
toberr <oberritter.tamas@gmail.com>
2017-03-05 00:06:46 +0100

    modify index page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,11 +2,14 @@
   <div class="inner index">
     <div class="row">
       <div class="small-12 medium-6 large-6 columns">
-        <p>Örökzöld, retro, mulatós élőben, és igény szerint laptopról a legmodernebb zenék is.<br>
-        <br>
-        Minden a Ti kívánságotok szerint! Teljes mértékben ránk is bízhatjátok, de közösen is összeállíthatjuk az est zenei anyagát, hogy a nagyszülőktől a barátokig, mindenki a legjobban érezhesse magát.<br>
-        <br>
-        Ha felkeltettük érdeklődéseteket, keressetek telefonon és <a href="mailto:okzenekar@gmail.com">emailen</a>.</p>
+        <p>Örökzöld, retro, mulatós élőben, és igény szerint napjaink zenéi DJ-szolgáltatással.</p>
+        <p>Minden a Ti kívánságotok szerint! Teljes mértékben ránk is bízhatjátok, de közösen is összeállíthatjuk az est zenei anyagát, hogy a nagyszülőktől a barátokig, mindenki a legjobban érezhesse magát.</p>
+        <p>Ajándék-szolgáltatásaink:</p>
+        <ul>
+          <li>a ceremóniamesteri tevékenység ellátása</li>
+          <li>a lakodalom helyszínen a esküvői szertartás kihangosítása</li>
+        </ul>
+        <p>Ha felkeltettük érdeklődéseteket, keressetek telefonon és <a href="mailto:okzenekar@gmail.com">emailen</a>.</p>
       </div>
       <div class="small-12 medium-6 large-6 columns">
         <img-cycle 
@@ -38,9 +41,32 @@ export default {
 
 <style lang="scss">
   @import '~root/components/scss/_variables';
-  @media #{$mobile} {
-    .index p{
+  .index {
+    p{
       margin-bottom: 10px;
+      &:last-child{
+        margin-bottom: 0;
+      }
+      @media #{$mobile} {
+        &:last-child{
+          margin-bottom: 10px;
+        }
+      }
+    }
+    ul{
+      margin-top: -10px;
+      margin-bottom: 10px;
+      li{
+        position: relative;
+        padding-left: 15px;
+        &:before{
+          position: absolute;
+          left: 0;
+          top: 4px;
+          content: '●';
+          line-height: 1;
+        }
+      }
     }
   }
 </style>
