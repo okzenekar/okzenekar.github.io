6f991af80328595f9279d932fb934c97d1280182
toberr <oberritter.tamas@gmail.com>
2017-05-06 14:15:40 -0400

    object assign polyfill

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 require('es6-promise').polyfill();
+require('root/components/polyfills');
 
 import Vue from 'vue';
 import VueRouter from 'vue-router';
@@ -19,5 +20,6 @@ var root = new Vue({
   render: h => h(app)
 });
 
-root.$mount('#app')
+Object.assign({}, {a: 1})
 
+root.$mount('#app')
